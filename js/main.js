178c49381f5d55cd4ceba1cf425924818ca9138b
Saul Florez <sherzo-b@hotmail.com>
2018-07-31 02:09:34 -0430

    User logued and categories

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+	
+	$('.dropdown-toggle').click(function(e) {
+	  if ($(document).width() > 768) {
+	    e.preventDefault();
+
+	    var url = $(this).attr('href');
+
+	       
+	    if (url !== '#') {
+	    
+	      window.location.href = url;
+	    }
+
+	  }
+	});
+})
